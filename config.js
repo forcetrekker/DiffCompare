fc886e2323760b17c84bcaee2854499438d39ee6
Rahul Sharma <rahuls91221@gmail.com>
2020-02-07 12:38:18 +0100

    Add comments to config.js

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 let config = {}
 
+// full path of source and destination folders for comparison, this can be hardcoded to reference exact folders
 let dirname = __dirname;
 config.path = {
     source: `${dirname}\\source`,
     destination: `${dirname}\\destination`
 };
 
-config.classNames = [
+// used for prompting user to select available configured file name to select from
+config.fileNames = [
     'AccountBuilder.cls',
     'EmailManager.cls'
 ];
